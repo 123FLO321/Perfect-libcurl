3a5759475b02991313456abf12a18221086db966
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-08-02 12:22:44 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,4 @@
-// swift-tools-version:4.0
-//
-//  Package.swift
-//  cURL
-//
-//  Created by Kyle Jessup on 3/22/16.
-//	Copyright (C) 2016 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
+// swift-tools-version:5.2
 
 import PackageDescription
 
